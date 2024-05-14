6db05d319974b0b71c8dbc81d5ad9828831629db
tungbeo2345 <fbaloch631pun0suy@hotmail.com>
2024-05-14 13:19:06 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const intersection = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => s.has(x));
-};
+function rob(nums) {
+  let prevMax = 0;
+  let currMax = 0;
+  for (const num of nums) {
+    const temp = currMax;
+    currMax = Math.max(prevMax + num, currMax);
+    prevMax = temp;
+  }
+  return currMax;
+}
